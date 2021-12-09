064aa3d52ec01a83088d4714e9a35c2650aa9658
Neil Ni <62610544+Hong4507@users.noreply.github.com>
2021-12-09 20:43:05 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const elements = [...document.querySelectorAll("span p:nth-child(1)")].reverse(),
+    mod = [1000, 60, 60, 24],
+    time = Date.parse("2022/1/1");
+const loop = () => {
+    var countdown = time - Date.now();
+    for (const key in elements) {
+        countdown = Math.floor(countdown / mod[key]);
+        elements[key].textContent = (mod[1 + Number(key)] ? countdown % mod[1 + Number(key)] : countdown).toString().padStart(2,0);
+    }
+    setTimeout(loop, 1000)
+}
+loop();
+
